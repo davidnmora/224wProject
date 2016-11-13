3e6c067a59022353a4de1d1e6d52ad3be1f99ca5
David Mora <davidnmora@gmail.com>
2016-11-12 20:28:21 -0800

    Created new file "circleSummaryStats.py" which gets summary statistics for each circle. UNFINISHED CURRENTLY. Currently just imports circles for a single ego network and loads each circle into a dict.

---
+++ All FB Data cs224w/circleSummaryStats.py
@@ -0,0 +1,31 @@
+from snap import *
+
+#DATA STRUCTURE_________________________
+#circle names mapped to a list of all the node ids in that circle
+circlesDict = dict() #key: "circle13", val: [id1, id2, ...]
+
+
+#LOAD CIRCLES INTO A MAP
+with open("facebook/0.circles", "r") as document:
+	line = document.readlines()
+	print len(line)
+	for i in range(len(line)):
+		print i
+		#split, then erase trailing '/n'
+		idList = line[i].split('\t') 
+		idList[len(idList)-1] = idList[len(idList)-1].rstrip('\n')
+		#set key (remove from array)
+		key = idList[0]
+		del idList[0]
+		#store key and val (array)
+		circlesDict[key] = idList
+	print circlesDict
+
+
+
+
+#CODE STORAGE BIN
+#LOAD AN EDGE LIST
+circleGraph = LoadEdgeList(PUNGraph, "facebook/0.edges", 0, 1)
+# G = LoadEdgeList(PUNGraph, "SMALL_imdb_actor_edges.tsv", 0, 1)
+print "G: Nodes %d, Edges %d" % (circleGraph.GetNodes(), circleGraph.GetEdges())
